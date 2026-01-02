3d51e7fc63a2f863bc6e699db97537df3953c802
ananya-codes-data <ananyapradhan7543@gmail.com>
2026-01-02 16:53:18 +0530

    Updated the sql practile file with new queries

--- Practice/14_Window_Functions_Basics.sql
+++ Practice/14_Window_Functions_Basics.sql
@@ -91,42 +91,196 @@ FROM orders;
 
 -- SQL WINDOW FUNCTIONS | FRAME CLAUSE
 
+SELECT
+	orderid,
+	orderdate,
+	orderstatus,
+	sales,
+	SUM(sales) OVER(
+		PARTITION BY 
+			orderstatus
+		ORDER BY
+			orderdate
+		ROWS BETWEEN CURRENT ROW AND 2 FOLLOWING
+	) AS total_sales
+FROM orders;
+
+SELECT
+	orderid,
+	orderdate,
+	orderstatus,
+	sales,
+	SUM(sales) OVER(
+		PARTITION BY 
+			orderstatus
+		ORDER BY
+			orderdate
+		ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW /* DEFAULT FRAME
+		, only works with the ORDER BY
+		, can be executed without it to get the exact result */
+		-- ROWS UNBOUNDED PRECEDING -- DEFAULT FRAME SHORT FORM
+		-- ROWS BETWEEN 2 PRECEDING AND CURRENT ROW
+		-- ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+		-- ROWS 2 PRECEDING
+		-- ROWS UNBOUNDED PRECEDING
+	) AS total_sales
+FROM orders;
+
 /* TASK 7: 
    Calculate Total Sales by Order Status for current and next two orders */
-
+SELECT
+	orderid,
+	orderdate,
+	orderstatus,
+	sales,
+	SUM(sales) OVER(
+		PARTITION BY
+			orderstatus
+		ORDER BY
+			orderdate
+		ROWS BETWEEN CURRENT ROW AND 2 FOLLOWING
+	) AS total_sales
+FROM orders;
 
 /* TASK 8: 
    Calculate Total Sales by Order Status for current and previous two orders */
-
+SELECT
+	orderid,
+	orderdate,
+	orderstatus,
+	sales,
+	SUM(sales) OVER(
+		PARTITION BY
+			orderstatus
+		ORDER BY
+			orderdate
+		ROWS BETWEEN 2 PRECEDING AND CURRENT ROW
+	) AS total_sales
+FROM orders;
 
 /* TASK 9: 
    Calculate Total Sales by Order Status from previous two orders only */
-
+SELECT
+	orderid,
+	orderdate,
+	orderstatus,
+	sales,
+	SUM(sales) OVER(
+		PARTITION BY
+			orderstatus
+		ORDER BY
+			orderdate
+		ROWS 2 PRECEDING
+	) AS total_sales
+FROM orders;
 
 /* TASK 10: 
    Calculate cumulative Total Sales by Order Status up to the current order */
-
+SELECT
+	orderid,
+	orderdate,
+	orderstatus,
+	sales,
+	SUM(sales) OVER(
+		PARTITION BY
+			orderstatus
+		ORDER BY
+			orderdate
+		ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+	) AS total_sales
+FROM orders;
 
 /* TASK 11: 
    Calculate cumulative Total Sales by Order Status from the start to the current row */
-
+SELECT
+	orderid,
+	orderdate,
+	orderstatus,
+	sales,
+	SUM(sales) OVER(
+		PARTITION BY
+			orderstatus
+		ORDER BY
+			orderdate
+		ROWS UNBOUNDED PRECEDING
+	) AS total_sales
+FROM orders;
 
 
 -- SQL WINDOW FUNCTIONS | RULES
 
 /* RULE 1: 
    Window functions can only be used in SELECT or ORDER BY clauses */
+SELECT
+	orderid,
+	orderdate,
+	orderstatus,
+	sales,
+	SUM(sales) OVER(
+		PARTITION BY
+			orderstatus
+	) AS total_sales
+FROM orders
+ORDER BY
+	SUM(sales) OVER(
+		PARTITION BY
+			orderstatus
+	) DESC;
 
-
--- Invalid: window function in WHERE clause
+/* Invalid: window functions are not allowed in WHERE clause as well as in GROUP BY clause
+window functions can't be used to filter data or group data */
 
 /* RULE 2: 
    Window functions cannot be nested */
+SELECT
+	orderid,
+	orderdate,
+	orderstatus,
+	sales,
+	SUM(SUM(sales) OVER(
+		PARTITION BY
+			orderstatus
+	)) OVER(
+		PARTITION BY
+			orderstatus
+	) AS total_sales
+FROM orders
 
--- Invalid nesting
+-- Error: window fuction cannot be nested
 
+/* Find the total sales for each order status,
+only for two products 101 and 102 */
+
+SELECT
+	orderid,
+	orderdate,
+	orderstatus,
+	productid,
+	sales,
+	SUM(sales) OVER(
+		PARTITION BY
+			orderstatus
+	) AS total_sales
+FROM orders
+WHERE
+	productid IN(101, 102);
+
+-- first filtering and then aggregation
 
 -- SQL WINDOW FUNCTIONS | GROUP BY
 
 /* TASK 12: 
    Rank customers by their total sales */
+SELECT
+	customerid,
+	SUM(sales) AS total_sales,
+	RANK() OVER(
+		ORDER BY
+			SUM(sales) DESC
+	) AS rank_customers
+FROM orders
+GROUP BY
+	customerid;
+
+/* We can use both GROUP BY and WINDOW Functions together but the only rule is,
+the columns that are to be used inside WINDOW functions needs to be in the GROUP BY too */
