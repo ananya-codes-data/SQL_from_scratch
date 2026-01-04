598b038490493ed51aaa2377bd82ec261dee44e2
ananya-codes-data <ananyapradhan7543@gmail.com>
2026-01-04 22:56:49 +0530

    Added new sql qeries to the file

--- Practice/15_Window_Aggregate_Functions.sql
+++ Practice/15_Window_Aggregate_Functions.sql
@@ -107,16 +107,39 @@ FROM orders;
    Find the Average Scores of Customers */
 SELECT
 	customerid,
+	lastname,
 	score,
+	COALESCE(score, 0) AS customer_score,
 	ROUND(
 		AVG(score) OVER(),
-		2
-	) AS avg_score
+		0
+	) AS avg_score,
+	ROUND(
+		AVG(
+			COALESCE(score, 0)
+			)
+		OVER(),
+		0
+	) AS avg_score_without_null
 FROM customers;
 
 /* TASK 8:
    Find all orders where Sales exceed the average Sales across all orders */
-
+SELECT
+*
+FROM(
+	SELECT
+		orderid,
+		productid,
+		sales,
+		ROUND(
+			AVG(sales) OVER(),
+			0
+		) AS avg_sales
+	FROM orders
+) AS t
+WHERE
+	sales > avg_sales;
 
 
 -- SQL WINDOW AGGREGATION | MAX / MIN
@@ -124,19 +147,55 @@ FROM customers;
 
 /* TASK 9:
    Find the Highest and Lowest Sales across all orders */
-
+SELECT
+	orderid,
+	orderdate,
+	sales,
+	MAX(sales) OVER() AS highest_sales,
+	MIN(sales) OVER() AS lowest_sales
+FROM orders;
 
 /* TASK 10:
-   Find the Lowest Sales across all orders and by Product */
-
+   Find the Highest and Lowest Sales across all orders and by Product */
+SELECT
+	orderid,
+	orderdate,
+	productid,
+	sales,
+	MAX(sales) OVER(
+		PARTITION BY
+			productid
+	) AS highest_sales_by_product,
+	MIN(sales) OVER(
+		PARTITION BY
+			productid
+	) AS lowest_sales_by_product
+FROM orders;
 
 /* TASK 11:
    Show the employees who have the highest salaries */
-
+SELECT
+*
+FROM (
+	SELECT
+		*,
+		MAX(salary) OVER () AS highest_salary
+	FROM employees
+) AS t
+WHERE
+	salary = highest_salary;
 
 /* TASK 12:
    Find the deviation of each Sale from the minimum and maximum Sales */
-
+SELECT
+	orderid,
+	orderdate,
+	sales,
+	MAX(sales) OVER() AS highest_sales,
+	MIN(sales) OVER() AS lowest_sales,
+	(MAX(sales) OVER() - sales) AS deviation_from_max,
+	(sales - MIN(sales) OVER()) AS deviation_from_min
+FROM orders;
 
 
 -- Use Case | ROLLING SUM & AVERAGE
@@ -144,8 +203,45 @@ FROM customers;
 
 /* TASK 13:
    Calculate the moving average of Sales for each Product over time */
-
+SELECT
+	orderid,
+	productid,
+	orderdate,
+	sales,
+	ROUND(
+		AVG(sales) OVER(
+			PARTITION BY
+				productid
+		),
+		2
+	) AS avg_by_product,
+	ROUND(
+		AVG(sales) OVER(
+			PARTITION BY
+				productid
+			ORDER BY
+				orderdate ASC
+		),
+		2
+	) AS moving_avg
+FROM orders;
 
 /* TASK 14:
    Calculate the moving average of Sales for each Product over time,
    including only the next order */
+SELECT
+	orderid,
+	productid,
+	orderdate,
+	sales,
+	ROUND(
+		AVG(sales) OVER(
+			PARTITION BY
+				productid
+			ORDER BY
+				orderdate ASC
+			ROWS BETWEEN CURRENT ROW AND 1 FOLLOWING
+		),
+		2
+	) AS rolling_avg
+FROM orders;
